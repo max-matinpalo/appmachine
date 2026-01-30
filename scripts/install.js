dff8b633dfe2382591003d0ddde0eae347f63945
max <max@maxs-MacBook-Air.local>
2026-01-30 11:54:50 +0200

    Update

--- scripts/install.js
+++ scripts/install.js
@@ -1,19 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
+
 function install() {
-	console.error('\x1b[32m%s\x1b[0m', '🚀 AppMachine: Installing workflows...');
-	const projectRoot = process.env.INIT_CWD || path.resolve('../../..');
-	const srcDir = path.join(__dirname, '../src');
-	const destDir = path.join(projectRoot, '.github/workflows');
-	const msgPath = path.join(__dirname, '../install-message.txt');
-	if (fs.existsSync(srcDir)) {
-		if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true });
-		fs.readdirSync(srcDir).forEach(file => {
-			if (file.endsWith('.yml')) fs.copyFileSync(path.join(srcDir, file), path.join(destDir, file));
+	const root = process.env.INIT_CWD || path.resolve('../../..');
+	const src = path.join(__dirname, '../src');
+	const dest = path.join(root, '.github/workflows');
+	const temp = path.join(__dirname, '../template');
+	const mach = path.join(root, 'appmachine');
+	if (fs.existsSync(src)) {
+		if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
+		fs.readdirSync(src).forEach(f => {
+			if (f.endsWith('.yml')) fs.copyFileSync(path.join(src, f), path.join(dest, f));
 		});
 	}
-	if (fs.existsSync(msgPath)) console.error('\n' + fs.readFileSync(msgPath, 'utf8') + '\n');
+	if (fs.existsSync(temp)) {
+		if (!fs.existsSync(mach)) fs.mkdirSync(mach, { recursive: true });
+		fs.readdirSync(temp).forEach(f => fs.copyFileSync(path.join(temp, f), path.join(mach, f)));
+	}
 }
+
 
 install();
