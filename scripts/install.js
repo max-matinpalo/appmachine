e31de4c69a542776051c6c98420bbbc8334d103e
max <max@maxs-MacBook-Air.local>
2026-01-30 10:21:43 +0200

    Update

--- scripts/install.js
+++ scripts/install.js
@@ -2,22 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 function install() {
-	console.log('install starts');
+	console.error('\x1b[32m%s\x1b[0m', '🚀 AppMachine: Installing workflows...');
 	const projectRoot = process.env.INIT_CWD || path.resolve('../../..');
 	const srcDir = path.join(__dirname, '../src');
 	const destDir = path.join(projectRoot, '.github/workflows');
 	const msgPath = path.join(__dirname, '../install-message.txt');
-
 	if (fs.existsSync(srcDir)) {
 		if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true });
 		fs.readdirSync(srcDir).forEach(file => {
-			const srcFile = path.join(srcDir, file);
-			const destFile = path.join(destDir, file);
-			if (file.endsWith('.yml')) fs.copyFileSync(srcFile, destFile);
+			if (file.endsWith('.yml')) fs.copyFileSync(path.join(srcDir, file), path.join(destDir, file));
 		});
 	}
-
-	if (fs.existsSync(msgPath)) console.log('\n' + fs.readFileSync(msgPath, 'utf8') + '\n');
+	if (fs.existsSync(msgPath)) console.error('\n' + fs.readFileSync(msgPath, 'utf8') + '\n');
 }
 
 install();
