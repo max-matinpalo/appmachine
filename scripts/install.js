d719c72f1a8ead6a79f588f14827ed2b42b0c401
max <max@maxs-MacBook-Air.local>
2026-01-30 11:56:25 +0200

    Update

--- scripts/install.js
+++ scripts/install.js
@@ -2,6 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 
+function copy(s, d) {
+	const stat = fs.statSync(s);
+	if (stat.isDirectory()) {
+		if (!fs.existsSync(d)) fs.mkdirSync(d, { recursive: true });
+		fs.readdirSync(s).forEach(f => copy(path.join(s, f), path.join(d, f)));
+		return;
+	}
+	fs.copyFileSync(s, d);
+}
+
+
 function install() {
 	const root = process.env.INIT_CWD || path.resolve('../../..');
 	const src = path.join(__dirname, '../src');
@@ -14,10 +25,7 @@ function install() {
 			if (f.endsWith('.yml')) fs.copyFileSync(path.join(src, f), path.join(dest, f));
 		});
 	}
-	if (fs.existsSync(temp)) {
-		if (!fs.existsSync(mach)) fs.mkdirSync(mach, { recursive: true });
-		fs.readdirSync(temp).forEach(f => fs.copyFileSync(path.join(temp, f), path.join(mach, f)));
-	}
+	if (fs.existsSync(temp)) copy(temp, mach);
 }
 
 
